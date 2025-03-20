c00eb2742d071b74e570b1d3f59a8010bedc7ca3
suhas1897 <chalamcharlasuhas911@gmail.com>
2025-03-20 14:48:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'https://indiumlabrecords.onrender.com/',
+    origin: 'https://indiumlabrecords.onrender.com',
     // origin: 'http://localhost:3000',
     
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
