6ede18d4de2b139a34907afbe4eebfb773fde5dd
suhas1897 <chalamcharlasuhas911@gmail.com>
2025-03-22 02:13:09 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'user'], required: true }
-});
+    role: { type: String, enum: ['admin', 'user', 'superadmin'], required: true },
+    status: { type: String, enum: ['pending', 'approved', 'removed'], default: 'pending' }
+  });
 
 module.exports = mongoose.model('User', UserSchema);
