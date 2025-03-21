6fae121ade05bd2edbbccd3eef392ac6be4740d4
suhas1897 <chalamcharlasuhas911@gmail.com>
2025-03-22 02:12:11 +0530

    Update Chemical.js

--- models/Chemical.js
+++ models/Chemical.js
@@ -3,14 +3,21 @@ const mongoose = require('mongoose');
 const ChemicalSchema = new mongoose.Schema({
   chemicalId: { type: String, required: true, unique: true },
   chemicalName: { type: String, required: true },
-  chemicalType: { type: String, required: true },
-  type: { type: String, required: true },
-  gramsAvailable: { type: Number, required: true },
-  make: { type: String, required: true },
+  chemicalType: { type: String, enum: ['Anhydrous', 'Hydrous'], required: true },
+  type: { type: String, enum: ['LR', 'AR'], required: true },
+  phase: { type: String, enum: ['Solid', 'Liquid', 'Gas'], required: true },
+  purity : {type: String },
+  quantityAvailable: { type: Number, required: true },
+  unit: { type: String, enum: ['g', 'mL', 'L'], required: true },
+  make: { type: String },
   dateOfMFG: { type: Date, required: true },
   dateOfExp: { type: Date, required: true },
+  purchase: { type: Number },
   purchaseDate: { type: Date },
-  purchase: { type: Number, required: false }, // Add purchase, optional for existing data
+  invoiceNumber: { type: String },
+  isAbsolute: { type: Boolean, default: false },
+  isApproximately: { type: Boolean, default: false },
+  rack: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Chemical', ChemicalSchema);
