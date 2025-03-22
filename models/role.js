7125b4b777c3a1e30c1b6dbe7171a2b14df1497f
suhas1897 <chalamcharlasuhas911@gmail.com>
2025-03-23 01:24:03 +0530

    Update role.js

--- models/role.js
+++ models/role.js
@@ -3,8 +3,8 @@ const User = require('./User'); // Adjust path to your User model
 // Fetch admin and super admin emails from MongoDB
 const getAdminEmails = async () => {
   try {
-    const admins = await User.find({ role: 'admin', status: 'approve' }).select('email');
-    const superAdmins = await User.find({ role: 'superadmin', status: 'approve' }).select('email');
+    const admins = await User.find({ role: 'admin', status: 'approved' }).select('email');
+    const superAdmins = await User.find({ role: 'superadmin', status: 'approved' }).select('email');
 
     const adminEmails = admins.map(admin => admin.email);
     const superAdminEmails = superAdmins.map(admin => admin.email);
