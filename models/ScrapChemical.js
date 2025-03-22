342dda4b1230e40927e2cd8735daf1f611a184bf
suhas1897 <chalamcharlasuhas911@gmail.com>
2025-03-23 00:56:39 +0530

    Update ScrapChemical.js

--- models/ScrapChemical.js
+++ models/ScrapChemical.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const scrapChemicalSchema = new Schema({
+const scrapRequestSchema = new mongoose.Schema({
   chemicalId: { type: String, required: true },
   chemicalName: { type: String, required: true },
   userName: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   date: { type: Date, default: Date.now },
-  status: { type: String, default: 'approved' }
-}, {
-  timestamps: true // Optional: adds createdAt and updatedAt fields
+  status: { 
+    type: String, 
+    enum: ['pending', 'admin-approved', 'approved', 'denied'], 
+    default: 'pending' 
+  },
+  scrapPhotoPath: { type: String, required: true },
 });
 
-module.exports = mongoose.model('ScrapChemical', scrapChemicalSchema);
+module.exports = mongoose.model('ScrapRequest', scrapRequestSchema);
