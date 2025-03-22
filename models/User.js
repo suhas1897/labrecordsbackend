d90c479b72c05e37034070afd99652d43909f8b5
suhas1897 <chalamcharlasuhas911@gmail.com>
2025-03-23 00:57:05 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,14 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['admin', 'user', 'superadmin'], required: true },
+    salutation: {type: String, enum: ['Dr', 'Ms', 'Mr', 'Mrs'], required: true},
+    designation: {type: String, enum: ['Principal_Investigator','Co_Principal_Investigator','Post_Doctoral_Fellow','Research Associate','Senior_Research_Fellow', 'Junior_Research_Fellow', 'Project Associate', 'Project Assistant','Research Scholar', 'Student Research Intern', 'Other'], required: true},
+    department: {type: String, required: true},
+    contactNumber: { type: String, required: true },
+    
+    joiningDate: { type: Date, required: true },
     status: { type: String, enum: ['pending', 'approved', 'removed'], default: 'pending' }
+
   });
 
 module.exports = mongoose.model('User', UserSchema);
